56df074039191290475f33680df354a8b33cac1b
daniloeder <daniloeder1997@gmail.com>
2023-10-09 15:46:45 -0300

    Changed some functions to adapt requests to new api pattern that uses django.contrib.gis.geos now

--- src/screens/Map/Map.js
+++ src/screens/Map/Map.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
-import MapView, { Marker, Callout } from 'react-native-maps';
+import MapView, { Marker, Callout, Circle } from 'react-native-maps';
 import * as Location from 'expo-location';
 import { StyleSheet, View, Text, Dimensions, Switch } from 'react-native';
 
@@ -9,237 +9,323 @@ import { API_AUTHORIZATION } from '@env';
 
 const width = Dimensions.get('window').width;
 
-const OnOffButton = ({ icon, isLocalEnabled, setIsLocalEnabled }) => {
-    return (
-        <View
-            onPress={() => {
+function calculateDistance(coord1, coord2) {
+  const R = 6371;
+  const lat1 = deg2rad(coord1.lat);
+  const lat2 = deg2rad(coord2.lat);
+  const lon1 = deg2rad(coord1.lng);
+  const lon2 = deg2rad(coord2.lng);
 
-            }}
-            style={{
-                width: width * 0.22,
-                height: width * 0.1,
-                backgroundColor: '#EEE',
-                borderRadius: width * 0.04,
-                marginBottom: width * 0.01,
-                flexDirection: 'row',
-                alignItems: 'center'
-            }}
-        >
-            <Icons name={icon} size={width * 0.08} style={{marginLeft: 4}} />
-            <Switch
-                value={isLocalEnabled}
-                onValueChange={() => setIsLocalEnabled(!isLocalEnabled)}
-                thumbColor={isLocalEnabled ? "#007bff" : "#ccc"}
-                trackColor={{ false: "#767577", true: "#81b0ff" }}
-                style={{ transform: [{ scaleX: 0.8 }, { scaleY: 0.8 }] }}
-            />
-        </View>
-    )
+  const dlat = lat2 - lat1;
+  const dlon = lon2 - lon1;
+
+  const a =
+    Math.sin(dlat / 2) ** 2 +
+    Math.cos(lat1) * Math.cos(lat2) * Math.sin(dlon / 2) ** 2;
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  const distance = (R * c) * 1000; // Distance in meters
+
+  return distance;
+}
+function deg2rad(deg) {
+  return deg * (Math.PI / 180);
 }
 
-const Map = ({ MAX_ZOOM_LATITUDE_DELTA = 0.045 * 1000, PATTERN_ZOOM_LATITUDE_DELTA = 0.01, SCROLL_ENABLED = true, ZOOM_ENABLED = true }) => {
+const OnOffButton = ({ icon, isLocalEnabled, setIsLocalEnabled }) => {
+  return (
+    <View
+      onPress={() => {
 
-    const [userLocation, setUserLocation] = useState(null);
-    const [places, setPlaces] = useState([]);
-    const [events, setEvents] = useState([]);
-    const mapRef = useRef();
+      }}
+      style={{
+        width: width * 0.22,
+        height: width * 0.1,
+        backgroundColor: '#EEE',
+        borderRadius: width * 0.04,
+        marginBottom: width * 0.01,
+        flexDirection: 'row',
+        alignItems: 'center'
+      }}
+    >
+      <Icons name={icon} size={width * 0.08} style={{ marginLeft: 4 }} />
+      <Switch
+        value={isLocalEnabled}
+        onValueChange={() => setIsLocalEnabled(!isLocalEnabled)}
+        thumbColor={isLocalEnabled ? "#007bff" : "#ccc"}
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        style={{ transform: [{ scaleX: 0.8 }, { scaleY: 0.8 }] }}
+      />
+    </View>
+  )
+}
 
-    const [isPlacesEnabled, setIsPlacesEnabled] = useState(true);
-    const [isEventEnabled, setIsEventEnabled] = useState(true);
+const CircleOverlay = ({ centerCoordinates, radius }) => {
+  return (
+    <Circle
+      center={centerCoordinates}
+      radius={radius}
+      fillColor="rgba(0, 0, 255, 0.1)" // You can customize the circle color and opacity here
+      strokeColor="rgba(0, 0, 255, 1)" // You can customize the circle border color and opacity here
+    />
+  );
+};
+const DoubleCircleOverlay = ({ centerCoordinates, radius }) => {
+  return (
+    <>
+      <CircleOverlay
+        centerCoordinates={centerCoordinates}
+        radius={radius}
+      />
+      <CircleOverlay
+        centerCoordinates={centerCoordinates}
+        radius={radius * 2}
+      />
+    </>
+  );
+};
 
-    useEffect(() => {
-        (async () => {
-            let { status } = await Location.requestForegroundPermissionsAsync();
-            if (status !== 'granted') {
-                console.error('Permission to access location was denied');
-                return;
-            }
 
-            let location = await Location.getCurrentPositionAsync({});
-            setUserLocation({
-                latitude: location.coords.latitude,
-                longitude: location.coords.longitude,
-                latitudeDelta: PATTERN_ZOOM_LATITUDE_DELTA,
-                longitudeDelta: PATTERN_ZOOM_LATITUDE_DELTA,
-            });
+const Map = ({ MAX_ZOOM_LATITUDE_DELTA = 0.045 * 1000, PATTERN_ZOOM_LATITUDE_DELTA = 0.01 * 8, SCROLL_ENABLED = true, ZOOM_ENABLED = true }) => {
+  const MAX_DISTANCE_METERS = 2000;
 
-            try {
-                const placesResponse = await fetch('http://192.168.0.118:8000/api/places/', {
-                    method: 'GET',
-                    headers: {
-                        'Authorization': `Token ${API_AUTHORIZATION}`
-                    }
-                });
-                if (!placesResponse.ok) {
-                    throw new Error('Network response was not ok' + placesResponse.statusText);
-                }
-                const placesData = await placesResponse.json();
-                setPlaces(placesData);
+  const [userLocation, setUserLocation] = useState(null);
+  const [places, setPlaces] = useState([]);
+  const [events, setEvents] = useState([]);
+  const mapRef = useRef();
 
-                const eventsResponse = await fetch('http://192.168.0.118:8000/api/events/', {
-                    method: 'GET',
-                    headers: {
-                        'Authorization': `Token ${API_AUTHORIZATION}`
-                    }
-                });
+  const [isPlacesEnabled, setIsPlacesEnabled] = useState(true);
+  const [isEventEnabled, setIsEventEnabled] = useState(true);
 
-                if (!eventsResponse.ok) {
-                    throw new Error('Network response was not ok' + eventsResponse.statusText);
-                }
-                const eventsData = await eventsResponse.json();
-                setEvents(eventsData);
-            } catch (error) {
-                console.error('There was a problem with the fetch operation: ', error);
-            }
-        })();
-    }, []);
+  const [globalCoordinates, setGlobalCoordinates] = useState({ lat: -15.480229647965352, lng: -42.19854198396206 });
+  const [coodinatesList, setCoordinatesList] = useState([{ lat: -15.480229647965352, lng: -42.19854198396206 }]);
 
-    const handleRegionChange = (newRegion) => {
-        console.log("Current Coordinates:", newRegion.latitude, newRegion.longitude);
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.error('Permission to access location was denied');
+        return;
+      }
 
-        if (newRegion.latitudeDelta > MAX_ZOOM_LATITUDE_DELTA) {
-            mapRef.current.animateToRegion({
-                ...newRegion,
-                latitudeDelta: MAX_ZOOM_LATITUDE_DELTA,
-                longitudeDelta: MAX_ZOOM_LATITUDE_DELTA * (newRegion.longitudeDelta / newRegion.latitudeDelta),
-            });
+      let location = await Location.getCurrentPositionAsync({});
+      setUserLocation({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: PATTERN_ZOOM_LATITUDE_DELTA,
+        longitudeDelta: PATTERN_ZOOM_LATITUDE_DELTA,
+      });
+
+      try {
+        const placesResponse = await fetch('http://192.168.0.118:8000/api/places/', {
+          method: 'GET',
+          headers: {
+            'Authorization': `Token ${API_AUTHORIZATION}`
+          }
+        });
+        console.log(placesResponse)
+        if (!placesResponse.ok) {
+          throw new Error('Network response was not ok' + placesResponse.statusText);
         }
-    };
+        const placesData = await placesResponse.json();
+        setPlaces(placesData);
 
+        const eventsResponse = await fetch('http://192.168.0.118:8000/api/events/', {
+          method: 'GET',
+          headers: {
+            'Authorization': `Token ${API_AUTHORIZATION}`
+          }
+        });
 
-    console.log(userLocation)
-    if (!userLocation) {
-        return null;
+        console.log(eventsResponse)
+        if (!eventsResponse.ok) {
+            throw new Error('Network response was not ok' + eventsResponse.statusText);
+        }
+        const eventsData = await eventsResponse.json();
+        setEvents(eventsData);
+      } catch (error) {
+        console.error('There was a problem with the fetch operation: ', error);
+      }
+    })();
+  }, []);
+
+  const handleRegionChange = (newRegion) => {
+    setGlobalCoordinates({ lat: newRegion.latitude, lng: newRegion.longitude })
+
+    // Check if there's any existing coordinate that is closer than MAX_DISTANCE_METERS
+    const isTooClose = coodinatesList.some((coordinate) => {
+      const distance = calculateDistance(
+        coordinate,
+        { lat: newRegion.latitude, lng: newRegion.longitude }
+      );
+      return distance < MAX_DISTANCE_METERS;
+    });
+
+    if (!isTooClose) {
+      setCoordinatesList([...coodinatesList, { lat: newRegion.latitude, lng: newRegion.longitude }]);
     }
 
-    return (
-        <View style={styles.container}>
-            <MapView
-                ref={mapRef}
-                style={styles.map}
-                initialRegion={userLocation}
-                onRegionChangeComplete={handleRegionChange}
-                scrollEnabled={SCROLL_ENABLED}
-                zoomEnabled={ZOOM_ENABLED}
-            >
-                {isPlacesEnabled && places.map((place) => {
-                    const [latitude, longitude] = place.coordinates.split(',').map(Number);
-                    return (
-                        <Marker key={place.id} coordinate={{ latitude, longitude }}>
-                            <Icons name="Gym" size={40} />
-                            <Callout tooltip={true} style={styles.calloutContainer}>
-                                <View style={styles.calloutView}>
-                                    <Text style={styles.calloutTitle}>{place.name}</Text>
-                                    <Text style={styles.calloutSubtitle}>Location: {place.location}</Text>
-                                    {/* Add any other details you want to show for a place */}
-                                </View>
-                            </Callout>
-                        </Marker>
-                    );
-                })}
-                {isEventEnabled && events.map((event) => {
-                    const [latitude, longitude] = event.coordinates.split(',').map(Number);
-                    return (
-                        <Marker key={event.id} coordinate={{ latitude, longitude }}>
-                            <Icons name="Run" size={40} />
-                            <Callout tooltip={true} style={styles.calloutContainer}>
-                                <View style={styles.calloutView}>
-                                    <Text style={styles.calloutTitle}>{event.title}</Text>
-                                    <Text style={styles.calloutSubtitle}>Location: {event.location}</Text>
-                                    <Text style={styles.calloutSubtitle}>Date & Time: {event.date_time}</Text>
-                                    <Text style={styles.calloutSubtitle}>Sport Type: {event.sport_type}</Text>
-                                </View>
-                            </Callout>
-                        </Marker>
-                    );
-                })}
-            </MapView>
-            <View style={styles.inputContainer}>
-                <GoogleAutocompletePicker />
-            </View>
-            <View
-                style={{
-                    width: width * 0.25,
-                    padding: 5,
-                    paddingBottom: 2,
-                    borderRadius: width * 0.01,
-                    backgroundColor: '#CCC',
-                    opacity: 0.9,
-                    position: 'absolute',
-                    right: width * 0.02,
-                    bottom: width * 0.05,
-                }}
-            >
-                <OnOffButton icon="Gym" isLocalEnabled={isPlacesEnabled} setIsLocalEnabled={setIsPlacesEnabled} />
-                <OnOffButton icon="Run" isLocalEnabled={isEventEnabled} setIsLocalEnabled={setIsEventEnabled} />
-            </View>
-        </View>
-    );
+    if (newRegion.latitudeDelta > MAX_ZOOM_LATITUDE_DELTA) {
+      mapRef.current.animateToRegion({
+        ...newRegion,
+        latitudeDelta: MAX_ZOOM_LATITUDE_DELTA,
+        longitudeDelta: MAX_ZOOM_LATITUDE_DELTA * (newRegion.longitudeDelta / newRegion.latitudeDelta),
+      });
+    }
+    if (newRegion.latitudeDelta > MAX_ZOOM_LATITUDE_DELTA) {
+      mapRef.current.animateToRegion({
+        ...newRegion,
+        latitudeDelta: MAX_ZOOM_LATITUDE_DELTA,
+        longitudeDelta: MAX_ZOOM_LATITUDE_DELTA * (newRegion.longitudeDelta / newRegion.latitudeDelta),
+      });
+    }
+  };
+
+  if (!userLocation) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        ref={mapRef}
+        style={styles.map}
+        initialRegion={userLocation}
+        onRegionChangeComplete={handleRegionChange}
+        scrollEnabled={SCROLL_ENABLED}
+        zoomEnabled={ZOOM_ENABLED}
+      >
+
+        <Marker coordinate={{ latitude: globalCoordinates.lat, longitude: globalCoordinates.lng }}>
+        </Marker>
+        {isPlacesEnabled && places.map((place) => {
+          const coordinatesArray = place.coordinates.match(/-?\d+\.\d+/g); // Extract numeric values from the string
+          const [longitude, latitude] = coordinatesArray.map(Number); // Convert to numbers
+          return (
+            <Marker key={place.id} coordinate={{ latitude, longitude }}>
+              <Icons name="Gym" size={40} />
+              <Callout tooltip={true} style={styles.calloutContainer}>
+                <View style={styles.calloutView}>
+                  <Text style={styles.calloutTitle}>{place.name}</Text>
+                  <Text style={styles.calloutSubtitle}>Location: {place.location}</Text>
+                  {/* Add any other details you want to show for a place */}
+                </View>
+              </Callout>
+            </Marker>
+          );
+        })}
+        {isEventEnabled && events.map((event) => {
+          const coordinatesArray = event.coordinates.match(/-?\d+\.\d+/g); // Extract numeric values from the string
+          const [longitude, latitude] = coordinatesArray.map(Number); // Convert to numbers
+          return (
+            <Marker key={event.id} coordinate={{ latitude, longitude }}>
+              <Icons name="Run" size={40} />
+              <Callout tooltip={true} style={styles.calloutContainer}>
+                <View style={styles.calloutView}>
+                  <Text style={styles.calloutTitle}>{event.title}</Text>
+                  <Text style={styles.calloutSubtitle}>Location: {event.location}</Text>
+                  <Text style={styles.calloutSubtitle}>Date & Time: {event.date_time}</Text>
+                  <Text style={styles.calloutSubtitle}>Sport Type: {event.sport_type}</Text>
+                </View>
+              </Callout>
+            </Marker>
+          );
+        })}
+
+        {coodinatesList.map((coordinates, index) =>
+          <DoubleCircleOverlay key={index}
+            centerCoordinates={{ latitude: coordinates.lat, longitude: coordinates.lng }}
+            radius={MAX_DISTANCE_METERS}
+          />
+        )}
+      </MapView>
+      <View style={styles.inputContainer}>
+        <GoogleAutocompletePicker />
+      </View>
+      <View
+        style={{
+          width: width * 0.25,
+          padding: 5,
+          paddingBottom: 2,
+          borderRadius: width * 0.01,
+          backgroundColor: '#CCC',
+          opacity: 0.9,
+          position: 'absolute',
+          right: width * 0.02,
+          bottom: width * 0.05,
+        }}
+      >
+        <OnOffButton icon="Gym" isLocalEnabled={isPlacesEnabled} setIsLocalEnabled={setIsPlacesEnabled} />
+        <OnOffButton icon="Run" isLocalEnabled={isEventEnabled} setIsLocalEnabled={setIsEventEnabled} />
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    map: {
-        flex: 1,
-    },
-    inputContainer: {
-        position: 'absolute',
-        top: 10,
-        left: 10,
-        right: 10,
-        borderRadius: 10,
-        flexDirection: 'row',
-    },
-    input: {
-        flex: 1,
-        padding: 10,
-        right: 5,
-        borderRadius: 10,
-        backgroundColor: 'white',
-    },
-    inputGo: {
-        width: 50,
-        height: 50,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 25,
-        backgroundColor: '#0c8ce9'
-    },
-    errorText: {
-        color: 'red',
-        textAlign: 'center',
-    },
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+  inputContainer: {
+    position: 'absolute',
+    top: 10,
+    left: 10,
+    right: 10,
+    borderRadius: 10,
+    flexDirection: 'row',
+  },
+  input: {
+    flex: 1,
+    padding: 10,
+    right: 5,
+    borderRadius: 10,
+    backgroundColor: 'white',
+  },
+  inputGo: {
+    width: 50,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 25,
+    backgroundColor: '#0c8ce9'
+  },
+  errorText: {
+    color: 'red',
+    textAlign: 'center',
+  },
 
 
 
-    calloutContainer: {
-        flexDirection: 'column',
-        alignSelf: 'flex-start',
-    },
-    calloutView: {
-        width: 200,
-        padding: 10,
-        borderRadius: 10,
-        backgroundColor: 'white',
-        borderColor: '#ccc',
-        borderWidth: 1,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.5,
-        shadowRadius: 2,
-        elevation: 2, // for Android
-    },
-    calloutTitle: {
-        color: '#333',
-        fontSize: 16,
-        fontWeight: 'bold',
-    },
-    calloutSubtitle: {
-        color: '#666',
-        fontSize: 14,
-    },
+  calloutContainer: {
+    flexDirection: 'column',
+    alignSelf: 'flex-start',
+  },
+  calloutView: {
+    width: 200,
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: 'white',
+    borderColor: '#ccc',
+    borderWidth: 1,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 2,
+    elevation: 2, // for Android
+  },
+  calloutTitle: {
+    color: '#333',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  calloutSubtitle: {
+    color: '#666',
+    fontSize: 14,
+  },
 });
 
 export default Map;
