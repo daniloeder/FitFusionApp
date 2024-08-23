b05d76fc18ee9cfe0e1236b65c5ba2d6c88aada6
daniloeder <daniloeder1997@gmail.com>
2024-08-23 17:59:20 -0300

    Managed to fix error that deny opening app with 'npx react-native run-android'

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+import { AppRegistry } from 'react-native';
 import { registerRootComponent } from 'expo';
-
 import App from './App';
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+if (!__DEV__) {
+    AppRegistry.registerComponent('FitFusionApp', () => App);
+} else {
+    registerRootComponent(App);
+}
